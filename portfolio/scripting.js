3fddf424d343f601af7d4116814377dfe4208464
Wouter Mostard <wouter.mostard@gmail.com>
2016-03-24 14:31:29 +0100

    Made 2 generic functions for mouseover and mouse leave.

--- portfolio/scripting.js
+++ portfolio/scripting.js
@@ -1,5 +1,41 @@
-//Event handles
-$('#portfolioButton').on("mouseenter",function(){
-    console.log("sdf");
-    $("#githubButton").hasClass("mini");
-})
+var textButtons =
+  [{id: "portfolioButton", text:"Portfolio tekst"},
+   {id: "githubButton", text:"Github tekst"},
+   {id: "aboutMeButton", text:"About me tekst"}
+  ]
+
+function handleMouseOver(Button){;
+  Button.siblings().animate({
+    height: "-=100px",
+    width: "-=100px"
+  });
+  Button.siblings().html("");
+  Button.animate({
+    height:"+=150px",
+    width:"+=450px"
+  },500)
+}
+
+function handleMouseLeave(Button){
+  Button.animate({
+    height: "-=150px",
+    width: "-=450px"
+  },500);
+  Button.siblings().each(function(){
+    for(var i =0; i < textButtons.length; i++){
+      if($(this).attr("id") == textButtons[i].id){
+        $(this).html(textButtons[i].text);
+      }
+    }
+  });
+  Button.siblings().animate({
+    height: "+=100px",
+    width: "+=100px"
+  });
+}
+$("#container").children().on("mouseover",function(){
+    handleMouseOver($(this));
+});
+$("#container").children().on("mouseleave",function(){
+    handleMouseLeave($(this));
+});
